e61317f9cb9a96a8866682fc3ede5ddeef5ef938
Duo Xu <xxxuduo@berkeley.edu>
2018-11-07 19:46:34 -0800

    lab part1 checked off

---
+++ iosFinalProject/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  iosFinalProject
+//
+//  Created by MacBook on 11/7/18.
+//  Copyright © 2018 MacBook. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
