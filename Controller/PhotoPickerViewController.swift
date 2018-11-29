dfc642e51c0e2b50591df58ee1a2dfb73b2df0cb
Collin Cao <collin@airbears2-10-142-64-39.airbears2.1918.berkeley.edu>
2018-11-28 19:10:48 -0800

    ready for bugfixs

--- Controller/PhotoPickerViewController.swift
+++ Controller/PhotoPickerViewController.swift
@@ -3,11 +3,30 @@ import UIKit
 class PhotoPickerViewController: PickerViewController {
     
     @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet var button: UIButton!
+    @IBOutlet var loader: UIActivityIndicatorView!
+    @IBOutlet weak var loaderWidthConstraint: NSLayoutConstraint!
     
+    var isProcessing = false
     var model = TransferStyle()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        var isProcessing : Bool = false {
+            didSet {
+                button.isEnabled = !isProcessing
+                self.isProcessing ? self.loader.startAnimating() : self.loader.stopAnimating()
+                self.loaderWidthConstraint.constant = self.isProcessing ? 20.0 : 0.0
+                UIView.animate(withDuration: 0.3) {
+                    self.isProcessing
+                        ? self.button.setTitle("Processing...", for: .normal)
+                        : self.button.setTitle("Transform!", for: .normal)
+                    self.view.layoutIfNeeded()
+                }
+            }
+        }
     }
     
     @IBOutlet weak var pickerBtn: UIButton!
@@ -37,7 +56,9 @@ class PhotoPickerViewController: PickerViewController {
             return
         }
         
+        self.isProcessing = true
         model.process(input: image) { filteredImage in
+            self.isProcessing = false
             if let filteredImage = filteredImage {
                 self.imageView.image = filteredImage
             }
